235fdcab39220148079ac30f651f8172c142aba3
Luffy524 <bharat152004@gmail.com>
2025-08-23 18:10:55 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Bharat Doshi",
+    author_email="bharat152004@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
